a1dddeb02b3b2e6b5322f54f9e2bc74162d74f7c
soldelotus <sdelotus@gmail.com>
2025-03-31 14:14:26 -0300

    Primeira parte da atividade

---
+++ main.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+salario = float(input("Digite seu salário: "))
+
+print(f"O seu nome é {nome}, você tem {idade} anos e recebe um salário de R${salario}")
+
+
