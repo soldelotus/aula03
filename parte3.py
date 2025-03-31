dcd68d31ad42a915199f62423f25c7b376e1fafa
soldelotus <sdelotus@gmail.com>
2025-03-31 15:59:06 -0300

    Finalização da quarta parte

---
+++ parte3.py
@@ -0,0 +1,12 @@
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+soma = n1+n2
+subtracao = n1-n2
+multiplicacao = n1*n2
+divisao = n1/n2
+
+print(f"O resultado da soma é {soma}"
+    f"\nO resultado da subtração é {subtracao}"
+    f"\nO resultado da multiplicação é {multiplicacao}"
+    f"\nO resultado da divisão é {divisao}")
---
+++ parte4.py
@@ -0,0 +1,10 @@
+A = 5
+B = 10
+
+print(f"O valor de A é {A} e o valor de B é {B}")
+
+aux = A
+A = B
+B = aux
+
+print(f"O novo valor de A é {A} e o novo valor de B é {B}")
