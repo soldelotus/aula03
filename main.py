133f0751312828ef35c95ff61ee37fb5bc147bc4
soldelotus <sdelotus@gmail.com>
2025-03-31 14:17:43 -0300

    Adicionando percentual de aumento

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 salario = float(input("Digite seu salário: "))
+porcentagem = int(input("Digite, em números, qual a porcentagem de aumento você recebeu: "))
 
-print(f"O seu nome é {nome}, você tem {idade} anos e recebe um salário de R${salario}")
+# salario / 100 x porcento
 
+aumento = (salario/100) * porcentagem
+novosalario = aumento + salario
 
+print(f"O seu nome é {nome}, você tem {idade} anos e recebia um salário de R${salario}")
+print(f"O seu novo salário, após os {porcentagem}% de aumento, é de R${novosalario}")
