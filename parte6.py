6396d24e26960ee8c8946b1f99f11f4366a11977
soldelotus <sdelotus@gmail.com>
2025-03-31 16:52:13 -0300

    Média com recuperação

--- parte6.py
+++ parte6.py
@@ -6,6 +6,8 @@ n3 = float(input("Digite a terceira nota: "))
 media = (n1 + n2 + n3) / 3
 
 if media >= 7.0 :
-    print(f"A média foi {media} e {aluno} foi aprovado(a)!")
+    print(f"A média foi {media:.2f} e {aluno} foi aprovado(a)!")
+elif (media >= 4):
+    print(f"A média foi {media:.2f} e {aluno} foi para recuperação.")
 else:
-    print(f"A média foi {media} e {aluno} foi reprovado(a).")
+    print(f"A média foi {media:.2f} e {aluno} foi para reprovado.")
